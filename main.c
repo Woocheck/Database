d937f95bc6c4bea1b7320fe80c6bdc17fd32a10f
Woocheck <woocheck@op.pl>
2019-10-27 11:46:08 +0100

    main function

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "./db_library/database.h"
 
@@ -8,7 +9,8 @@ int main(int argc, char const *argv[])
     struct DataBase * local_db;
     local_db = malloc( sizeof( struct DataBase ) );
 
-    InitDataBase( DATA_BASE_FILE, local_db );
+    InitDataBase( local_db );
     
+    free( local_db );
     return 0;
 }
